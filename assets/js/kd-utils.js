cf448a4b417f2c86b438cdf4aabd9ea36ae5d763
Kevin deLeon <kevin.deleon@gmail.com>
2017-01-15 11:24:50 -0600

    update outbound link detection

--- assets/js/kd-utils.js
+++ assets/js/kd-utils.js
@@ -59,4 +59,10 @@ var KDUtils = {
 		} 
 		document.cookie = name + "=" + value + expires + "; path=/" + domain; 
 	},
+	isExternal: function(url) {
+		var match = url.match(/^([^:\/?#]+:)?(?:\/\/([^\/?#]*))?([^?#]+)?(\?[^#]*)?(#.*)?/);
+		if (typeof match[1] === "string" && match[1].length > 0 && match[1].toLowerCase() !== location.protocol) return true;
+		if (typeof match[2] === "string" && match[2].length > 0 && match[2].replace(new RegExp(":("+{"http:":80,"https:":443}[location.protocol]+")?$"), "") !== location.host) return true;
+		return false;
+	}
 };
